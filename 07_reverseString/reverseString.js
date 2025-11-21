101ae89acc367570755e8cd24c40edcdbc54d5ac
KaufmWhiskas <philippdiener4@gmail.com>
2025-11-21 13:25:55 +0100

    add function to reverse String given

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newStr = ""
+    for (let i = 0; i < str.length; i++)
+        newStr = str[i] + newStr
+    return newStr
 };
+
+
 
 // Do not edit below this line
 module.exports = reverseString;
