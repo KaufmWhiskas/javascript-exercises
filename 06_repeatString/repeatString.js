d3f814418cfd2f4a47525623a86de0d222a63fd2
KaufmWhiskas <philippdiener4@gmail.com>
2025-11-21 13:20:35 +0100

    complete exercise 06 with a for loop

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, amount) {
+    if (amount < 0){
+        return "ERROR"
+    }
+    outputString = ""
+    for (let i = 0; i < amount; i++){
+        outputString += str
+    }
+    return outputString
 };
 
 // Do not edit below this line
