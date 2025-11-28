06ca3484b48b5c3fe00403ca6a7ffe04305d1ceb
KaufmWhiskas <philippdiener4@gmail.com>
2025-11-28 10:38:00 +0100

    add scripts to solve different maths equations

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return (n1 - n2)
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((sum, number) => {
+    return sum + number
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((result, number) => {
+    return result * number
+  });
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return (base ** exponent)
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+	for(let i = 1; i <= number; i++){
+    result = result * i
+  };
+  return result;
 };
 
 // Do not edit below this line
