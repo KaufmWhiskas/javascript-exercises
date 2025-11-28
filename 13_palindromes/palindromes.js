095c8ef5cd96f7c12fc3159ca4732efbfa5fe046
KaufmWhiskas <philippdiener4@gmail.com>
2025-11-28 10:54:02 +0100

    add script to check for palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replaceAll(" ", "").replaceAll(/[^\w\s]|_/g, "").toLowerCase();
+    for(let i = 0; i < string.length; i++){
+        if (string[i] !== string[string.length - 1 - i]){
+            return false
+        };
+    };
+    return true
 };
 
 // Do not edit below this line
