2653801cbba45e1f57c80f5ad351e18b25497f7c
KaufmWhiskas <philippdiener4@gmail.com>
2025-11-28 12:27:42 +0100

    add script to return oldest person out of array

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest, person) => {
+        let personAge = 0
+        let oldestAge = 0
+        if(!oldest.yearOfDeath){
+            oldestAge = new Date().getFullYear() - oldest.yearOfBirth;
+        } else {
+            oldestAge = oldest.yearOfDeath - oldest.yearOfBirth
+        }
+        if(!person.yearOfDeath){
+            personAge = new Date().getFullYear() - person.yearOfBirth;
+        } else {
+        personAge = person.yearOfDeath - person.yearOfBirth;
+        };
+        if (personAge > oldestAge){
+            return person;
+        };
+        return oldest
+    }, array[0]);
 };
 
 // Do not edit below this line
